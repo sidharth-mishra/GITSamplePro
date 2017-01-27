c33b6297af251548beb36352bb366545ee10ceab
sidharth-mishra <mishrasidhartha4@gmail.com>
2017-01-27 11:33:36 +0530

    Intial Version

---
+++ src/main/java/com/tyfone/mcb/service/GitSimpleProjApplication.java
@@ -0,0 +1,12 @@
+package com.tyfone.mcb.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSimpleProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSimpleProjApplication.class, args);
+	}
+}
